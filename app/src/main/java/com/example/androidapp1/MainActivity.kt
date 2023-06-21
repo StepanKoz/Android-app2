a7c88e65d91964544c0eb299502dab8fbf7cff84
Stepan <121403794+StepanKoz@users.noreply.github.com>
2023-06-21 19:43:58 +0400

    First commit

---
+++ app/src/main/java/com/example/androidapp1/MainActivity.kt
@@ -0,0 +1,158 @@
+//package com.example.androidapp1
+//
+//import android.os.Bundle
+//import androidx.activity.ComponentActivity
+//import androidx.activity.compose.setContent
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.material3.MaterialTheme
+//import androidx.compose.material3.Surface
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.tooling.preview.Preview
+//import com.example.androidapp1.ui.theme.AndroidApp1Theme
+//
+//class MainActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContent {
+//            AndroidApp1Theme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    Greeting("Android")
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    AndroidApp1Theme {
+//        Greeting("Android")
+//    }
+//}
+//
+//class Person constructor(firstName: String) { /*...*/ }
+//
+//fun main () {
+//    val data = time()
+//    table(data)
+//    things()
+//    time()
+//    table()
+//}
+//
+//fun things () {
+//
+//    println("Write your things")
+//    val input = readLine()
+//    println("Your bag $input")
+//}
+//
+//
+//
+//fun time () {
+//
+//    println("Write how often we will send to you nofications")
+//    println("Hours system")
+//    val hours = readLine()?.toInt()
+//    println("Minutes system")
+//    val input = readLine()
+//    val minutes = input?.toInt()
+//    var isMessagePrinted = false
+//}
+//
+//fun table (time) {
+//
+//    var isMessagePrinted = false
+//    while (true) {
+//        if (minutes != null && hours != null) {
+//            if (!isMessagePrinted) {
+//                println("Enter correct parameter")
+//
+//                isMessagePrinted = true }
+//        } else {
+//            break
+//        }
+//    }
+//}
+//class
+
+
+
+//    while (true) {
+//
+//        val hours = readLine()?.toInt()
+//        val input = readLine()
+//        val minutes = input?.toInt()
+//        if (minutes != null && hours != null) {
+//            if ((minutes < 0 || minutes > 59) || (hours < 0 || hours > 24)) {
+//                println("Enter correct parameter")
+//                continue
+//            } else {
+//                break
+//            }
+//        }
+
+
+
+
+
+
+
+
+
+
+//    if (minutes != null && hours != null) {
+//        if ((minutes < 0 || minutes > 59 ) || (hours < 0 || hours > 24)) {
+//            println("Enter correct parameter")
+//        } else {
+//            println("Cool!")
+//            println("Now your things will be next to you everytime")
+//        }
+//
+//    } else {
+//        println("Enter correct parameter")
+//    }
+
+
+
+
+
+
+
+
+
+
+class InitOrderDemo() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+class MainActivity {
+
+}
